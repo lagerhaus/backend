bcb96292e1e288f69c44dda70ad080e2b3b08121
T0astBread <tb@t0ast.cc>
2019-05-20 11:06:43 +0200

    Finish database schema

--- create_database.sql
+++ create_database.sql
@@ -1,6 +1,9 @@
 DROP TABLE fruit CASCADE;
 DROP TABLE ripeness CASCADE;
+DROP TABLE region CASCADE;
 DROP TABLE batch CASCADE;
+DROP TABLE weather CASCADE;
+
 
 CREATE TABLE fruit (
 	fruit_id INTEGER PRIMARY KEY,
@@ -17,6 +20,13 @@ CREATE TABLE ripeness (
 	UNIQUE(name, fruit_id)
 );
 
+CREATE TABLE region (
+	region_id INTEGER PRIMARY KEY,
+	name VARCHAR UNIQUE,
+	area VARCHAR,
+	level INTEGER
+);
+
 CREATE TABLE batch (
 	batch_id INTEGER PRIMARY KEY,
 	fruit_id INTEGER,
@@ -24,9 +34,23 @@ CREATE TABLE batch (
 	month INTEGER,
 	amount INTEGER,
 	storage_date DATE,
+	region_id INTEGER,
 	ripeness_id INTEGER,
 
 	FOREIGN KEY (fruit_id) REFERENCES fruit(fruit_id),
+	FOREIGN KEY (region_id) REFERENCES region(region_id),
 	FOREIGN KEY (ripeness_id) REFERENCES ripeness(ripeness_id),
 	UNIQUE(fruit_id, year, month)
+);
+
+CREATE TABLE weather (
+	weather_id INTEGER PRIMARY KEY,
+	region_id INTEGER,
+	year INTEGER,
+	month INTEGER,
+	rainy_days INTEGER,
+	sunny_days INTEGER,
+	
+	FOREIGN KEY (region_id) REFERENCES region(region_id),
+	UNIQUE(region_id, year, month)
 );
