03411c57002c5fe713f84c3d9971c1c6945e5b25
buchj <buchj2@gmail.com>
2019-06-03 12:34:33 +0200

    Add GET /weather Card : https://github.com/lagerhaus/backend/projects/2#card-22018685

---
+++ server/LagerhausServer/DTOs/WeatherDTO.cs
@@ -0,0 +1,27 @@
+﻿using LagerhausDb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LagerhausServer.DTOs
+{
+    public class WeatherDTO
+    {
+        public String Region { get; set; }
+        public int? Year { get; set; }
+        public int? Month { get; set; }
+        public int? RainyDays { get; set; }
+        public int? SunnyDays { get; set; }
+
+
+        public WeatherDTO(Weather w)
+        {
+            this.Region = w.Region.Name;
+            this.Year = w.Year;
+            this.Month = w.Month;
+            this.RainyDays = w.RainyDays;
+            this.SunnyDays = w.SunnyDays;           
+        }
+    }
+}
