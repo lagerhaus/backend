a86fac629bd90a80eab1a40eea89b9d9a2ba597b
T0astBread <tb@t0ast.cc>
2019-06-24 05:38:27 +0200

    Remove start_prod_postgres script
    This is now handled by the deploy wizard
    
    See https://github.com/lagerhaus/deploy

--- db/start_prod_postgres.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo Set up the new database user
-read -p "Username: " pg_user
-read -sp "Password: " pg_pass
-echo
-
-docker run --rm --name lhaus-postgres -e POSTGRES_USER=$pg_user -e POSTGRES_PASSWORD=$pg_pass -d -p 5432:5432 -v $(pwd)/pg_data:/var/lib/postgresql/data postgres
